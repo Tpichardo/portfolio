e3927d731a51d4707c64a4f7737bcb7896811b53
Talia Pichardo <taliapichardo@gmail.com>
2022-01-18 22:38:55 -0500

    Adds self image and about component

---
+++ src/Components/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import selfImg from '../Assets/HomeImg.png'
+
+const About = () => {
+    return (
+        <Section>
+            <img src="selfImg" alt="An image of me smiling" />
+            <p>Hi, I’m Talía! A Full Stack Web Developer
+                and Fitness Enthusiast
+            </p>
+        </Section>
+    )
+}
+
