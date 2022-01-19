9403be467992e09bb18717adedd7441e4f63abd9
Talia Pichardo <taliapichardo@gmail.com>
2022-01-18 22:44:07 -0500

    Adds edits to About and imports it to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import NavBar from './Components/NavBar';
+import About from './Components/About';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <main>
+        <About />
+      </main>
     </div>
   );
 }
--- src/Components/About.js
+++ src/Components/About.js
@@ -3,12 +3,14 @@ import selfImg from '../Assets/HomeImg.png'
 
 const About = () => {
     return (
-        <Section>
-            <img src="selfImg" alt="An image of me smiling" />
+        <section>
+            <img src={selfImg} alt="An image of me smiling" />
             <p>Hi, I’m Talía! A Full Stack Web Developer
                 and Fitness Enthusiast
             </p>
-        </Section>
-    )
+        </section>
+    );
 }
+
+export default About;
 
