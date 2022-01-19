98d7bf22b6955dc9863a93dcdb8735a511361666
Talia Pichardo <taliapichardo@gmail.com>
2022-01-18 21:59:38 -0500

    Imports PT Sans Google font

--- src/App.js
+++ src/App.js
@@ -4,20 +4,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Almost before we knew it</h1>
+      <ul>
+        <li>this</li>
+        <li>is</li>
+        <li>a</li>
+        <li>simple</li>
+        <li>list</li>
+      </ul>
     </div>
   );
 }
