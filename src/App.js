8608c4157eb9004c140d8698588ed2f55a2d0c78
Talia Pichardo <taliapichardo@gmail.com>
2022-01-19 00:56:02 -0500

    Adds contact, skills, and projects components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import NavBar from './Components/NavBar';
 import { Routes, Route } from 'react-router-dom';
 import Home from './Components/Home';
 import About from './Components/About';
+import Skills from './Components/Skills-Tools';
+import Projects from './Components/Projects';
+import Contact from './Components/Contact';
 
 function App() {
   return (
@@ -12,6 +15,9 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
+          <Route path="/skills-tools" element={<Skills />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
       </main>
     </div>
