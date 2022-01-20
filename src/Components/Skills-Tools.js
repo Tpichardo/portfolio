2b418f89c59919537c525d9779e2ac94ef58f4d4
Talia Pichardo <taliapichardo@gmail.com>
2022-01-20 06:07:02 -0500

    changes class to className for devicons

--- src/Components/Skills-Tools.js
+++ src/Components/Skills-Tools.js
@@ -5,20 +5,20 @@ const Skills = () => {
         <section className="skills-tools" id="skills-tools">
             <h1>My Toolbox & skills</h1>
             <div className="toolbox">
-                <i class="devicon-html5-plain"></i>
-                <i class="devicon-css3-plain"></i>
-                <i class="devicon-javascript-plain"></i>
-                <i class="devicon-react-original"></i>
-                <i class="devicon-nodejs-plain"></i>
-                <i class="devicon-postgresql-plain"></i>
-                <i class="devicon-vscode-plain"></i>
-                <i class="devicon-bootstrap-plain"></i>
-                <i class="devicon-git-plain"></i>
-                <i class="devicon-github-original"></i>
-                <i class="devicon-vscode-plain"></i>
-                <i class="devicon-figma-plain"></i>
-                <i class="devicon-trello-plain"></i>
-                <i class="devicon-firebase-plain"></i>
+                <i className="devicon-html5-plain"></i>
+                <i className="devicon-css3-plain"></i>
+                <i className="devicon-javascript-plain"></i>
+                <i className="devicon-react-original"></i>
+                <i className="devicon-nodejs-plain"></i>
+                <i className="devicon-postgresql-plain"></i>
+                <i className="devicon-vscode-plain"></i>
+                <i className="devicon-bootstrap-plain"></i>
+                <i className="devicon-git-plain"></i>
+                <i className="devicon-github-original"></i>
+                <i className="devicon-vscode-plain"></i>
+                <i className="devicon-figma-plain"></i>
+                <i className="devicon-trello-plain"></i>
+                <i className="devicon-firebase-plain"></i>
             </div>
             <h3>Currently Working on:</h3>
             <p className="currently">
