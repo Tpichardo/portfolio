96af5f1f522c08acae8a379aadc72a8a9f717ff9
Talia Pichardo <taliapichardo@gmail.com>
2022-02-03 10:21:08 -0500

    Imports desktopNav and mobilenav

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,98 +1,15 @@
 import React from 'react';
-import { Link } from 'react-scroll';
+import DesktopNav from './DesktopNav';
+import MobileNav from './MobileNav';
 import './NavBar.css'
 
 const NavBar = () => {
     return (
-        <header>
-            <div className="logo">
-                <h1>
-                    <Link
-                        activeClass="active"
-                        to="home"
-                        spy={true}
-                        smooth={true}
-                        duration={500}
-                    >
-                        TP
-                    </Link>
-                </h1>
-            </div>
-            <nav>
-                <Link className="hamburger_menu hide-for-desktop">
-                    <span className="header_span"></span>
-                    <span className="header_span"></span>
-                    <span className="header_span"></span>
-                </Link>
-                <ul className='primary-nav hide-for-mobile'>
-                    <li>
-                        <Link
-                            activeClass="active"
-                            to="home"
-                            spy={true}
-                            smooth={true}
-                            duration={500}
-                            className="nav-item"
-                        >
-                            Home
-                        </Link>
-                    </li>
-                    <li>
-                        <Link
-                            activeClass="active"
-                            to="about"
-                            spy={true}
-                            smooth={true}
-                            duration={500}
-                            className="nav-item"
-                        >
-                            About
-                        </Link>
-                    </li>
-                    <li>
-                        <Link
-                            activeClass="active"
-                            to="skills-tools"
-                            spy={true}
-                            smooth={true}
-                            duration={500}
-                            className="nav-item"
-                        >
-                            Skills
-                        </Link>
-                    </li>
-                    <li>
-                        <Link
-                            activeClass="active"
-                            to="projects"
-                            spy={true}
-                            smooth={true}
-                            duration={500}
-                            className="nav-item"
-                        >
-                            Projects
-                        </Link >
-                    </li>
-                    <li>
-                        <Link
-                            activeClass="active"
-                            to="contact"
-                            spy={true}
-                            smooth={true}
-                            duration={500}
-                            className="nav-item"
-                        >
-                            Contact
-                        </Link>
-                    </li>
-                    <li onClick={() => window.open('https://drive.google.com/file/d/1_udUkEmVZC4Sg7m-ZSx6hiWNhy5-Xj2b/view?usp=sharing')}
-                        className="nav-item resume"
-                    >
-                        Resume
-                    </li>
-                </ul>
-            </nav>
-        </header>);
+        <div>
+            <DesktopNav />
+            <MobileNav />
+        </div>
+    );
 }
 
 export default NavBar;
