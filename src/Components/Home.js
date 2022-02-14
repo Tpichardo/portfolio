40bfc05db2195c75c6b8b11783e26c20f67e0536
Talia Pichardo <taliapichardo@gmail.com>
2022-02-14 18:38:10 -0500

    Adds typewriter component

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import selfImg from '../Assets/LandingImg.jpg';
+import selfImg from '../Assets/LandingImg.png';
+import Typewriter from 'typewriter-effect';
 import './Home.css'
 
 const Home = () => {
@@ -9,8 +10,17 @@ const Home = () => {
                 <img src={selfImg} alt="me smiling" />
             </div>
             <div className="home-content">
-                <h1>Hi, I’m Talía! A Full Stack Web Developer
-                    and <span>Fitness Enthusiast</span>
+                <h1>Hi, I’m Talía! A Full Stack Web Developer and
+                    <span className='typeWriter'>
+                        <Typewriter
+                            options={{
+                                strings: ["Fitness Enthusiast", "Creator", "Nature Lover", "Collaborator", "Go-getter"],
+                                autoStart: true,
+                                pauseFor: 250,
+                                loop: true
+                            }}
+                        />
+                    </span>
                 </h1>
             </div>
         </section>
