7db98f7f76adcbf5613705c568a3a889c5222cbf
Talia Pichardo <taliapichardo@gmail.com>
2022-02-15 16:30:39 -0500

    Refactors layout for better responsiveness

--- src/Components/Skills-Tools.js
+++ src/Components/Skills-Tools.js
@@ -1,30 +1,46 @@
 import React from 'react';
+import './Skills-Tools.css'
 
 const Skills = () => {
     return (
         <section className="skills-tools" id="skills-tools">
-            <h1>My Toolbox & skills</h1>
+            <h1 className='myToolsHeader'>My Toolbox & skills</h1>
             <div className="toolbox">
                 <i className="devicon-html5-plain"></i>
+
+
                 <i className="devicon-css3-plain"></i>
+
+
                 <i className="devicon-javascript-plain"></i>
+
+
                 <i className="devicon-react-original"></i>
+
+
                 <i className="devicon-nodejs-plain"></i>
+
                 <i className="devicon-postgresql-plain"></i>
+
+
                 <i className="devicon-vscode-plain"></i>
+
+
                 <i className="devicon-bootstrap-plain"></i>
+
+
                 <i className="devicon-git-plain"></i>
+
+
                 <i className="devicon-github-original"></i>
-                <i className="devicon-vscode-plain"></i>
-                <i className="devicon-figma-plain"></i>
+
+                {/* <div className='devicon'>
+                    <i className="devicon-vscode-plain"></i>
+                </div>
+                <div className='devicon'>
+                    <i className="devicon-figma-plain"></i>
+                </div> */}
             </div>
-            <h3>Currently Working on:</h3>
-            <p className="currently">
-                Improving my CSS skills with Kevin Powell’s “Conquering Responsive Layouts” course and learning Data Structures & Algorithms.
-                <span onClick={() => window.open('https://twitter.com/PichardoTalia')}>
-                    Follow my journey here!
-                </span>
-            </p>
         </section>
     );
 }
