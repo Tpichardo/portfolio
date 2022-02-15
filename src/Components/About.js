527e7258d1bf0fdfd75d75ddb7e8c789893c16f2
Talia Pichardo <taliapichardo@gmail.com>
2022-02-14 20:47:11 -0500

    Adds new about-me

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import HikingImg from '../Assets/HikingImg.jpg'
+import HikingImg from '../Assets/HikingImg.jpg';
+import './About.css'
 
 const About = () => {
     return (
@@ -8,22 +9,40 @@ const About = () => {
                 <img src={HikingImg} alt="Me looking out to the mountains" />
             </div>
             <div className="about-content">
-                <header>
-                    <h1>¡Hola!</h1>
-                </header>
+                <h1>
+                    <h1 className='hola'>¡Hola!</h1>
+                </h1>
                 <p>
-                    ’m a Dominican Full Stack Web Developer with a background in IT.
-                    I have an interest in motivational tech, and I’m excited to continue creating applications that motivate individuals
-                    to achieve their goals and establish healthy habits.
+                    I'm glad that you're here!
                 </p>
+
                 <p>
-                    When I’m not coding, you can find me spending time with family and friends,
-                    reading on my kindle,dancing to EVERYTHING, and bike riding.
+                    My name is <span>Talía</span>, and I’m a recent Pursuit grad, so I’m currently enjoying a new path to
+                    self-expression as a <span>Full Stack Web Developer</span>.
                 </p>
+
+                <p>
+                    Being a student of life inspires me to continue to grow and learn new skills.
+                    I'm now working on my CSS skills by rebuilding my portfolio website with tips from
+                    Kevin Powell's Conquering Responsive Layouts course.
+                    I'm documenting the process on Twitter with the 100DaysOfCode challenge.
+                    You can follow me <span className='twitter' onClick={() => window.open('https://twitter.com/PichardoTalia')}>@PichardoTalia</span> to see how I'm doing!
+                </p>
+
+                <p>
+                    To excite and inspire my community, I want to be a member of an organization that shares my ideals.
+                    As a developer, I intend to channel this enthusiasm into code that encourages people to
+                    <span> achieve their goals and adopt healthy habits</span>.
+                </p>
+
+                <p>
+                    When I'm not coding, you'll find me with my family and friends, dancing to anything,
+                    watching self-help YouTube videos, working out, or reading on my new Kindle!
+                </p>
+
                 <p>
                     Feel free to reach out and say hello!
                 </p>
-                <button className="about-connect">Let's Connect</button>
             </div>
         </section>
     )
